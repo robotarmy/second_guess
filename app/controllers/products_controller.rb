5fd63c7d4d1f7a4c03609a0e6459eda4300bf729
Curtis Schofield <curtis@ram9.cc>
2012-04-19 12:21:12 -0700

    started to inttegrate views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  before_filter :authenticate_user!
+  def new
+  end
+  def create
+    product_attr = {
+      :creator_user => current_user
+    }.reverse_merge(params[:product])
+
+  end
 end
