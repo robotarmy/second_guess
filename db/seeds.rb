f2e0adb9a0398f22c91a38b9e0ceaf811caf7664
Curtis Schofield <curtis@ram9.cc>
2012-04-20 09:50:33 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Product.create!(:name => 'FizBangWallGotcha', 
+                :description => 'In the night, without any sight
+                                 you have to jump over walls -
+                                 each wall you do not run into
+                                 brightens the alley that you
+                                 run in - Each  time you hit a
+                                 wall without jumping - Fiz Bang
+                                 Wall Gotcha! - The ambient light
+                                 level drops slowly if you stand
+                                 still or do not jump a wall.')
+
+Product.create!(:name => 'CapitalCakeBattle',
+                :description => "You have a battleship made out of
+                                 cake. It is sailing on an ocean
+                                 of burnt milk. You encounter
+                                 a cake shark - made of  cake
+                                 that must eat a part of  your ship
+                                 to continue to make it's cake-flesh
+                                 body. While this is happening, your
+                                 Cake Battle Ship friends are trying
+                                 to create their own cake sharks to
+                                 fetch either the natural cake shark
+                                 or parts of your boat.")
+
+Product.create!(:name => 'LambertsNaturalShoeCollection', 
+                :description => "Lambert is a small biz owner who
+                                 quit his job at a giant corporation
+                                 to create a competitive product of
+                                 natural shoes. 
+                                 In this Adventure you will have to
+                                 collaborate with supply chains, 
+                                 build network connections, all while
+                                 trying to stay in the black. Eventually
+                                 you get the notice of mega-shooo-corp
+                                 and have to decide to sell or continue
+                                 the fight.
+                                 Lambert is a Lamb of course ")
+
