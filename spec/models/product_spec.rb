b901e22fd254ddfaa9ff112915638589e1b58871
Curtis Schofield <curtis@ram9.cc>
2012-04-20 11:30:02 -0700

    updated  test case

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,27 +1,32 @@
 require 'spec_helper'
 
 describe Product do
-  it "has a vote count works with negative" do
+  let(:user) do
     user = User.create(
             :email => 'c@r9.c',
             :password => 'hihihi',
             :password_confirmation => 'hihihi')
+
+  end
+  let(:user2) do
+    user2 = User.create(
+            :email => '1@r9.c',
+            :password => 'hihihi',
+            :password_confirmation => 'hihihi')
+  end
+  it "has a vote count works with negative" do
     p = Product.create!(:name => 'One')
     p.votes.should be_empty
     p.votes.create(:user_id => user.id, :value => -1)
-    p.votes.create(:user_id => user.id, :value => -1)
+    p.votes.create(:user_id => user2.id, :value => -1)
     p.negative_vote_count.should == 2
   end
 
   it "has a vote count works with positive" do
-    user = User.create(
-            :email => 'c@r9.c',
-            :password => 'hihihi',
-            :password_confirmation => 'hihihi')
     p = Product.create!(:name => 'One')
     p.votes.should be_empty
     p.votes.create(:user_id => user.id, :value => 1)
-    p.votes.create(:user_id => user.id, :value => 1)
+    p.votes.create(:user_id => user2.id, :value => 1)
     p.positive_vote_count.should == 2
   end
 
