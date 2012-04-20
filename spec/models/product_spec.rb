7dc2e69c355d07effed53f4b25e91c29e528acd8
Curtis Schofield <curtis@ram9.cc>
2012-04-20 10:17:03 -0700

    positive_vote_count negative_vote_count

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
 
 describe Product do
+  it "has a vote count works with positive" do
+    user = User.create(
+            :email => 'c@r9.c',
+            :password => 'hihihi',
+            :password_confirmation => 'hihihi')
+    p = Product.create!(:name => 'One')
+    p.votes.should be_empty
+    p.votes.create(:user_id => user.id, :value => -1)
+    p.votes.create(:user_id => user.id, :value => -1)
+    p.negative_vote_count.should == 2
+  end
 
+  it "has a vote count works with positive" do
+    user = User.create(
+            :email => 'c@r9.c',
+            :password => 'hihihi',
+            :password_confirmation => 'hihihi')
+    p = Product.create!(:name => 'One')
+    p.votes.should be_empty
+    p.votes.create(:user_id => user.id, :value => 1)
+    p.votes.create(:user_id => user.id, :value => 1)
+    p.positive_vote_count.should == 2
+  end
   it "has a creator " do
     user = User.create(
             :email => 'c@r9.c',
